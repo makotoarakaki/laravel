dba9ada5ca93ff8215b9f469183f0efe024aabd4
Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>
2019-11-18 15:36:00 +0900

    using layouts & add header footer
    Signed-off-by: Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,12 @@
-@if (session('message'))
+@extends('layouts.layouts')
+
+@section('title', 'Simple Board')
+
+@section('content')
+    @if (session('message'))
         {{ session('message') }}
-@endif
+    @endif
 
-{{ $post->title }}
-{{ $post->content }} 
-
-<a href="/posts/{{ $post->id }}/edit">Edit</a>
-
+    {{ $post->title }}
+    {{ $post->content }}
+@endsection 
