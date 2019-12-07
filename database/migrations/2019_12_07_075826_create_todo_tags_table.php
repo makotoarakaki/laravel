17d1ece527f2f56f876fd9b19d0ced0c2ae70044
Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>
2019-12-07 17:07:12 +0900

    impl relation
    Signed-off-by: Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>

---
+++ database/migrations/2019_12_07_075826_create_todo_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_todo', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('todo_id');
+            $table->integer('tag_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_tags');
+    }
+}
