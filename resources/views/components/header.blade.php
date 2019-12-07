3db059eebbd97f0c0777678c0797213463f19e14
Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>
2019-12-07 16:33:58 +0900

    add auth check
    Signed-off-by: Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,6 @@
 <header class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-  <a href="/" class="navbar-brand">TODO App</a>
+  <a href="/laravel/goals" class="navbar-brand">TODO App</a>
+  
   <button
     class="navbar-toggler"
     type="button"
@@ -11,18 +12,22 @@
   >
     <span class="navbar-toggler-icon"></span>
   </button>
-  @if (Route::has('login'))
-                &nbsp;&nbsp;
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-      @endif
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            @auth
+            <li class="nav-item">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+            </li>
+            @else
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @endauth
+        </ul>
+    </div>
 </header>
