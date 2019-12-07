4b815426e6a631eacc2906b3e1ef40b12f33fd2e
Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>
2019-12-07 11:36:35 +0900

    set routing
    Signed-off-by: Makoto Arakaki <m_arakaki@MakotonoMacBook-ea.local>

--- routes/web.php
+++ routes/web.php
@@ -28,19 +28,11 @@ Route::get('/', 'HomeController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-//Route::get('/', 'GoalController@index');
-
 Route::resource("goals", "GoalController");
 
 Route::resource("goals.todos", "TodoController");
 
 Route::post('/goals/{goal}/todos/{todo}/sort', 'TodoController@sort');
-
-// Route::resource("goals", "GoalController")->middleware('auth');
-
-// Route::resource("goals.todos", "TodoController")->middleware('auth');
-
-// Route::post('/goals/{goal}/todos/{todo}/sort', 'TodoController@sort')->middleware('auth');
 
 Auth::routes();
 
